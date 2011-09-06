a3f7862394c9607d2bade715a192cf1a899274dc
Israel Canasa <raeldc@gmail.com>
2011-09-06 18:21:39 +0800

    Chapters model's id should be filtered as "cmd"

--- administrator/components/com_learn/models/chapters.php
+++ administrator/components/com_learn/models/chapters.php
@@ -1,3 +1,12 @@
 <?php
 
-class ComLearnModelChapters extends ComLearnModelNosql{}
+class ComLearnModelChapters extends ComLearnModelNosql
+{
+	public function __construct(KConfig $config)
+	{
+		parent::__construct($config);
+            
+        // Insert as id as unique
+        $this->_state->reset()->insert('id', 'cmd', null, true);
+	}	
+}
