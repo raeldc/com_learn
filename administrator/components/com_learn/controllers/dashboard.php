7030310e545c0cce250d5b46d11159f4561e1865
Israel Canasa <raeldc@gmail.com>
2011-09-06 19:26:06 +0800

    Dashboard controller should just get the dashboard model instead of its plural name.

--- administrator/components/com_learn/controllers/dashboard.php
+++ administrator/components/com_learn/controllers/dashboard.php
@@ -1,3 +1,14 @@
 <?php
 
-class ComLearnControllerDashboard extends ComLearnControllerPage{}
+class ComLearnControllerDashboard extends ComLearnControllerPage
+{
+	protected function _initialize(KConfig $config)
+	{
+		$config->append(array(
+			'model' => 'com://admin/learn.model.dashboard',
+		));
+	
+		parent::_initialize($config);
+	}
+	
+}
