b4d8da49cd99a949aa1dc54eb3f809016658f959
Israel Canasa <raeldc@gmail.com>
2011-09-07 23:52:18 +0800

    Added gistit template filter
    This will convert <a href="url-to-gistit">script</a> to <script
    src="url-to-gistit"></script>

---
+++ administrator/components/com_learn/templates/filters/gistit.php
@@ -0,0 +1,20 @@
+<?php
+
+class ComLearnTemplateFilterGistit extends KTemplateFilterAbstract implements KTemplateFilterWrite
+{
+    protected function _initialize(KConfig $config)
+    {
+        $config->append(array(
+            'priority' => KCommand::PRIORITY_LOWEST,
+        ));
+        
+        parent::_initialize($config);
+    }
+
+    public function write(&$text)
+    {
+        $text = preg_replace('#<a.*href="http://gist\-it\.appspot\.com/(.*)".*>[sS]cript</a>#iU', '<div class="-script-wrapper"><script src="http://gist-it.appspot.com/$1"></script></div>', $text);
+
+        return $this;
+    }  
+}
