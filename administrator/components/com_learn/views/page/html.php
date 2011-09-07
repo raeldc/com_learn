cd0b946a488b2e5087b66bf81653febb5e151925
Israel Canasa <raeldc@gmail.com>
2011-09-07 12:44:43 +0800

    Added Example of how markdown document is rendered

--- administrator/components/com_learn/views/page/html.php
+++ administrator/components/com_learn/views/page/html.php
@@ -5,7 +5,7 @@ class ComLearnViewPageHtml extends ComLearnViewMarkdown
 	protected function _initialize(KConfig $config)
 	{
 		$config->append(array(
-			'layout' => 'md://admin/com.learn.docs.pages.C0101',
+			'layout' => 'md://admin/com.learn.docs.pages.01-01',
 		));
 	
 		parent::_initialize($config);
