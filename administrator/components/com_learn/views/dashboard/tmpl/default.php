c2193c6e7378beb7b6a8af2700b724b30251d203
Israel Canasa <raeldc@gmail.com>
2011-09-08 22:13:04 +0800

    Improved CSS

--- administrator/components/com_learn/views/dashboard/tmpl/default.php
+++ administrator/components/com_learn/views/dashboard/tmpl/default.php
@@ -1,17 +1,14 @@
+<script src="media://com_learn/js/scrollable.js" />
 <style src="media://com_learn/css/default.css" />
 
-<div id="chapter-box">
+<div id="chapter-box" class="-scrollable">
 	<?=@template('com://admin/learn.view.chapters.default')?>
 </div>
 
-<div id="pages-box">
+<div id="pages-box" class="-scrollable">
 	<?=@template('com://admin/learn.view.pages.default')?>
 </div>
 
-<div id="page-box">
+<div id="page-box" class="-scrollable">
 	<?=KFactory::get('com://admin/learn.controller.page')->page($state->page)->display();?>
-</div>
-
-<div id="tips-box">
-
 </div>
